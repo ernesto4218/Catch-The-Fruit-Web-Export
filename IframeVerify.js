97ac598fd98ef91a1d9211168c3849b0bfb4a8a0
ernesto4218 <84199830+ernesto4218@users.noreply.github.com>
2022-12-13 19:40:29 +0800

    Update IframeVerify.js

--- IframeVerify.js
+++ IframeVerify.js
@@ -1,21 +1,3 @@
-	<script>
-	window.onload = function() {
-	  // Run your JavaScript code here
-	// Check for the presence of the iframe element
-	if (window.self !== window.top) {
-	  // The website is being accessed inside an iframe, so check the value of the Referer header
-	  var referer = document.referrer;
-
-	  if (referer === "https://nesgentgames.com/") {
-	    // The website is being accessed inside an iframe on the specified page, so allow access to the content
-	  } else {
-	    // The website is not being accessed inside an iframe on the specified page, so block access to the content
-		window.location.replace("http://www.w3schools.com");
-	  }
-	} else {
-	  // The website is not being accessed inside an iframe, so block access to the content
-	}
-
-	}
-
-</script>
+window.onload = function() {
+  window.location.replace("http://www.w3schools.com");
+}
